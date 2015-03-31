a2bdbbab5f38daa41e6830ff06aeeb5221f520e7
Matt Neil <mneil@wi.rr.com>
2015-03-30 19:00:11 -0500

    first commit of exa-child

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function exa_child_scripts()
+{
+    wp_enqueue_style('exa-style', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('exa-child-style', get_stylesheet_directory_uri().'/style.css', array('exa-style'));
+    /*
+    if (is_page("revelry"))
+    {
+        wp_enqueue_script("exa-child-revelry-script", get_stylesheet_directory_uri().'/revelry/revelry.js', array('jquery'));
+    }
+    */
+}
+add_action('wp_enqueue_scripts', 'exa_child_scripts');
