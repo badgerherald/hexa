30a182e40383c9c024ffcfde4d1c3ccf5f75f6e1
Will Haynes <haynes24@gmail.com>
2015-11-13 20:59:12 -0600

    Move ad initiation here.

--- inc/functions/ads.php
+++ inc/functions/ads.php
@@ -11,16 +11,19 @@
 /**
  * Setup doubleclick breakpoints and network codes.
  * 
- * @see https://github.com/willhaynes/DoubleClick-for-Wordpress
+ * @see https://github.com/inn/DoubleClick-for-Wordpress
  */
 function hexa_ad_setup() {
 
 	global $DoubleClick;
-	$DoubleClick->networkCode = "8653162";
 
-	if( !hrld_is_production() )
-		$DoubleClick->debug = "64222555";
-
+	if( hrld_is_production() ) {
+		// Production networkCode:
+		$DoubleClick->networkCode = "8653162";
+	} else {
+		// Test networkCode:
+		$DoubleClick->networkCode = "64222555";		// Ask Will Haynes for access.
+	}
 
 }
 add_action('dfw_setup','hexa_ad_setup');
