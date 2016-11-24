63e2fe60866bac9e5139f6c6e2a35ef14fa6099c
Will Haynes <haynes24@gmail.com>
2016-11-24 10:49:22 -0500

    Small updates to advertising page

--- inc/containers/advertise-hero.php
+++ inc/containers/advertise-hero.php
@@ -5,31 +5,21 @@ $container->default_args(
 	array('navonly'=>false)
 );
 
-if(!$container->args['navonly']):
+$navonly = $container->args['navonly'];
 ?>
 
-<div class="<?php echo $container->classes(); ?>">
+<div class="<?php echo $container->classes($navonly ? 'navonly' : ''); ?>">
 	<div class="wrapper">
 		<h1>Advertise with <span class="the">the</span> Badger Herald</h1>
+		<?php if(!$navonly) : ?>
 		<p><span>Founded in 1969, The Badger Herald is a college media platform built by students</span></p>
 		<p><span>as an alternative voice for students at the University of Wisconsin-Madison.</span></p>
-		<p class="seller"><span>We can connect your brand to the campus market.</span></p>
+		<p class="seller"><span>We can connect your brand on campus</span></p>
+		<?php endif; ?>
 	</div>
 </div>
 
 <?php
-
-else:
-
-?>
-<div class="<?php echo $container->classes(); ?> navonly">
-	<div class="wrapper">
-		<h1>Advertise with <span class="the">the</span> Badger Herald</h1>
-	</div>
-</div>
-
-<?
-endif;
 
 $container = new container('advertise-nav');
 
@@ -37,16 +27,11 @@ $container = new container('advertise-nav');
 
 <div class="<?php echo $container->classes(); ?>">
 	<div class="wrapper">
-
-	<?php 
-
-	$args = array( 'theme_location' => 'ad-nav', );
-	wp_nav_menu($args); 
-
-	?>
-
+	<?php wp_nav_menu(array( 'theme_location' => 'ad-nav', )); ?>
 	<div class="clearfix"></div>
 	</div>
-
 </div>
+
+
+
 
