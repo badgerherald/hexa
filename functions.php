86a97ba847a6791b3e0b156dafd2c189cec760fb
Will Haynes <haynes24@gmail.com>
2016-11-24 11:01:29 -0500

    Add style cache bust, remove stale code

--- functions.php
+++ functions.php
@@ -13,14 +13,8 @@ include_once('inc/functions/plugin-wp_nav_menu_extended.php');
  * Enqueue hexa scripts and styles.
  */
 function hexa_scripts() {
-    wp_enqueue_style('hexa-style', get_stylesheet_directory_uri().'/style.css', array('exa-style'),'v1');
-    if (is_page("revelry"))
-    {
-        wp_enqueue_script("exa-child-revelry-script", get_stylesheet_directory_uri().'/revelry/revelry.js', array('jquery'));
-        wp_enqueue_style('hrld-showcase-style');
-        wp_enqueue_script( 'hrld-showcase-script-class');
-        wp_enqueue_script('exa-hrld-showcase-init', get_template_directory_uri().'/js/hrld-showcase-init.js', array('hrld-showcase-script-class', 'jquery'));
-    }
+    $mtime = file_exists(dirname(__FILE__) . '/style.css') ? filemtime(dirname(__FILE__) . '/style.css') : "";
+    wp_enqueue_style('hexa-style', get_stylesheet_directory_uri().'/style.css', array('exa-style'),$mtime);
 }
 add_action('wp_enqueue_scripts', 'hexa_scripts');
 
