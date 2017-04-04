7fc758ff01bb6ee6d37fa9a7825a8c9a6219030c
Will Haynes <haynes24@gmail.com>
2017-04-03 20:08:40 -0500

    target the right name

--- inc/functions/dirty-bird.php
+++ inc/functions/dirty-bird.php
@@ -7,7 +7,7 @@
 function hexa_dirty_bird_template($single_template) {
      global $post;
 
-     if ( exa_get_topic($post) == 'dirty-bird' ) {
+     if ( exa_get_topic($post) == 'Dirty Bird' ) {
 		$single_template = get_stylesheet_directory() . '/post-dirtybird.php';
      }
 
